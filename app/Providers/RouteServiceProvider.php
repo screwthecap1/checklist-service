6663aa8509b806fe6ad83375c55407fb33450478
screwthecap1 <chanskiy777@mail.ru>
2025-07-06 13:42:55 +0400

    Update Models&Provider connection

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->group(base_path('routes/api.php'));
+
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    }
+}
+
